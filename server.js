d74b14344374e17e41c2d4afa3a2d9f923d9e3db
Erik Van Slyke <elv180000@utdallas.edu>
2021-04-02 00:54:38 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+	res.sendfile('index.html');
+})
+
+app.get('/landing.js', (req, res) => {
+	res.sendfile('landing.js');
+})
+
+app.get('/two.js', (req, res) => {
+	res.sendfile('two.js');
+})
+
+app.listen(port, () => {
+	console.log(`listening at http://localhost${port}`);
+})
