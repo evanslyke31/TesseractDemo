72a5cb5e1c2a998af1793209cf1b4fc2d9a5f456
Erik Van Slyke <evanslyke31@gmail.com>
2021-04-10 21:03:02 -0500

    upload working

--- upload.js
+++ upload.js
@@ -5,9 +5,6 @@ dropArea.addEventListener('dragleave', handlerFunction, false)
 dropArea.addEventListener('dragover', handlerFunction, false)
 dropArea.addEventListener('drop', drop, false)
 
-function handleFiles(files) {
-    ([...files]).forEach(uploadFile)
-  }
 
 function handlerFunction(evt) {
     evt.preventDefault();
@@ -19,15 +16,25 @@ function drop(evt) {
     let dt = evt.dataTransfer
     let files = dt.files
 
-    handleFiles(files)
+    uploadFile(files[0])
 }
-
-function handleFiles(files) {
-    ([...files]).forEach(uploadFile)
-}
-
 function uploadFile(file) {
     console.log(file);
     uploading = true;
+    fetch('https://localhost:44385/api/PDFCompressor', { // Your POST endpoint
+        method: 'POST',
+        headers: {
+        // Content-Type may need to be completely **omitted**
+        // or you may need something
+        "Content-Type": " multipart/form-data"
+        },
+        body: file // This is your file object
+    }).then(
+        response => console.log(response.text()) // if the response is a JSON object
+    ).then(
+        success => console.log(success) // Handle the success response object
+    ).catch(
+        error => console.log(error) // Handle the error response object
+    );
     //$('.modal').modal('show');
   }
